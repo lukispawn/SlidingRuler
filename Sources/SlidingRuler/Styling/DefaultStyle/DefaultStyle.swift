8d72240707e0b1a9e3b601f15f89dcdff52625d6
Łukasz Zajdel <lukasz.zajdel.spawn@gmail.com>
2023-10-16 10:25:34 +0200

    bug fixings: support custom style, fix runtime error

--- Sources/SlidingRuler/Styling/DefaultStyle/DefaultStyle.swift
+++ Sources/SlidingRuler/Styling/DefaultStyle/DefaultStyle.swift
@@ -32,6 +32,8 @@ import SwiftUI
 public struct PrimarySlidingRulerStyle: SlidingRulerStyle {
     public let cursorAlignment: VerticalAlignment = .top
 
+    public init() {}
+    
     public func makeCellBody(configuration: SlidingRulerStyleConfiguation) -> some FractionableView {
         DefaultCellBody(mark: configuration.mark,
                         bounds: configuration.bounds,
